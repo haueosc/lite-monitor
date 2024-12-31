8ea0fcbdd92373d55cc95ebbbe5eacfee906d543
Doge2077 <1002302241@qq.com>
2024-12-31 22:09:01 +0800

    feat:add report config

---
+++ lite-monitor-server/src/main/java/com/example/config/ReportConfiguration.java
@@ -0,0 +1,11 @@
+package com.example.config;
+
+/**
+*@author : Doge2077 22:05 2024/12/31
+*/
+public class ReportConfiguration {
+    public static final class DefaultConfig {
+        Double cpuUsage = 0.8;
+        Double cacheUsage = 0.8;
+    }
+}
